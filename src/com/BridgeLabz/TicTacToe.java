a8178b755a135d48e98b2d8456c60064784bed38
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-25 01:15:27 +0530

    Adding the method so user can move in gameboard according to the position

--- src/com/BridgeLabz/TicTacToe.java
+++ src/com/BridgeLabz/TicTacToe.java
@@ -5,6 +5,7 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        char letter;
         System.out.println("Welcome to the tic tac toe");
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
@@ -13,12 +14,16 @@ public class TicTacToe {
                 {' ', '|', ' ', '|', ' '}};
         printGameBoard(gameBoard);
         System.out.println("Enter the letter X or o");
-        char letter = input.next().charAt(0);
+        letter = input.next().charAt(0);
+        System.out.println("Enter the position between 1 to 9");
+        int pos = input.nextInt();
+        placePiece(gameBoard,pos,letter);
     }
+
     //method to show the board to the player so player can move properly
-    public static void printGameBoard(char[][] gameBoard){
-        for(char[] row : gameBoard){
-            for(char c : row){
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
                 System.out.print(c);
             }
             System.out.println();
@@ -26,4 +31,27 @@ public class TicTacToe {
 
     }
 
+    public static void placePiece(char[][] gameBoard,int pos, char letter){
+        switch (pos){
+            case 1: gameBoard[0][0]= letter;
+                break;
+            case 2: gameBoard[0][2] = letter;
+                break;
+            case 3: gameBoard[0][4] = letter;
+                break;
+            case 4: gameBoard[2][0] = letter;
+                break;
+            case 5: gameBoard[2][2] = letter;
+                break;
+            case 6: gameBoard[2][4] = letter;
+                break;
+            case 7: gameBoard[4][0] = letter;
+                break;
+            case 8: gameBoard[4][2] = letter;
+                break;
+            case 9: gameBoard[4][4] = letter;
+                break;
+        }
+        printGameBoard(gameBoard);
+    }
 }
